bbf2abb54e14daa713e152a8208eff65b54edf3f
Eugene <eugen.rabii@gmail.com>
2021-08-10 10:52:34 -0400

    first

---
+++ src/main/java/zero/x/App.java
@@ -0,0 +1,10 @@
+package zero.x;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
